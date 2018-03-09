cdac814ab91a522f71e56a35b563961a30d97202
Nicolas Spencer <nicolaspencer@gmail.com>
2018-03-09 13:12:43 -0300

    Don't run script when imported. De esta forma si importamos el archivo, no se ejecuta lo que esta como main

--- meteors/find_meteors.py
+++ meteors/find_meteors.py
@@ -21,26 +21,28 @@ def calc_dist(lat1, lon1, lat2, lon2):
 def get_dist(meteor):
     return meteor.get('distance', math.inf)
 
-my_loc = (29.4244122, -98.493628)
+if __name__ == '__main__':
 
-#Con la libreria request se va a buscar con get y devuelve un string (meteor_resp)
-meteor_resp = requests.get('https://data.nasa.gov/resource/y77d-th95.json')
-#se transforma estos datos a formato json. Ahora meteor_data es una lista (list)
-#donde cada elemento posee los atributos de un meteorito
-meteor_data = meteor_resp.json()
+    my_loc = (29.4244122, -98.493628)
 
-# se realiza un for donde por cada meteor en la lista, si existen los atributos
-# reclat y reclong dentro del diccionario dentro de cada elemento de la lista,
-# se agrega el elemento 'distance' dentro del diccionario que se calcula entre
-# la tupla my_loc y la coordenada de cada meteorito.
-for meteor in meteor_data:
-    if not ('reclat' in meteor and 'reclong' in meteor): continue
-    meteor['distance'] = calc_dist(my_loc[0], my_loc[1], float(meteor['reclat']), float(meteor['reclong']))
+    #Con la libreria request se va a buscar con get y devuelve un string (meteor_resp)
+    meteor_resp = requests.get('https://data.nasa.gov/resource/y77d-th95.json')
+    #se transforma estos datos a formato json. Ahora meteor_data es una lista (list)
+    #donde cada elemento posee los atributos de un meteorito
+    meteor_data = meteor_resp.json()
 
-# se ordenan los elementos de la lista meteor_data. Se pasa una funcion (get_dist)
-# para ordenar los elementos por el atributo 'distance'.
-meteor_data.sort(key=get_dist)
+    # se realiza un for donde por cada meteor en la lista, si existen los atributos
+    # reclat y reclong dentro del diccionario dentro de cada elemento de la lista,
+    # se agrega el elemento 'distance' dentro del diccionario que se calcula entre
+    # la tupla my_loc y la coordenada de cada meteorito.
+    for meteor in meteor_data:
+        if not ('reclat' in meteor and 'reclong' in meteor): continue
+        meteor['distance'] = calc_dist(my_loc[0], my_loc[1], float(meteor['reclat']), float(meteor['reclong']))
 
-# se imprimen los primeros 10 elementos que representan los meteoritos mas
-# cerca a my_loc
-print(meteor_data[0:10])
+    # se ordenan los elementos de la lista meteor_data. Se pasa una funcion (get_dist)
+    # para ordenar los elementos por el atributo 'distance'.
+    meteor_data.sort(key=get_dist)
+
+    # se imprimen los primeros 10 elementos que representan los meteoritos mas
+    # cerca a my_loc
+    print(meteor_data[0:10])
